52730125c3f276e10b0fee78818690f87d5d7939
Jan Colab <jan040793gmail.com>
2022-01-13 23:21:07 +0000

    visual sugar

--- app.py
+++ app.py
@@ -4,28 +4,40 @@
 from flask import Flask, render_template , request
 from flask_ngrok import run_with_ngrok
 from anytree.importer import JsonImporter
+import numpy as np
 
 importer = JsonImporter()
 with open('/content/drive/MyDrive/g_projects/OMEN/stroco/tree_out.json') as f:
   root = importer.read(f)
 
-app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
+def update_app_data(app_data , splitValue):
+  app_data['history'] += splitValue
+  return app_data
 
 app_data = {
     "html_title":   "OMEN metaverse stroco",
-    "root": root.name
+    "root": root,
+    "history": root.name + '<br><br>',
+    "cnode": root,
+    "children_name": [x.name for x in root.children]
 }
 
-@app.route('/')
+app = Flask(__name__)
+run_with_ngrok(app)  # Start ngrok when app is run
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
-    if request.form.get('action1') == 'VALUE1':
-        pass # do something
-    elif  request.form.get('action2') == 'VALUE2':
-        pass # do something else
-    else:
-        pass # unknown
+    if not (request.form.get('splitValue') is None):
+        # app_data = update_app_value(app_data , request.form.get('splitValue'))
+        selectID = np.where([request.form.get('splitValue') == x[:20] for x in app_data['children_name']])[0][0]
+        app_data['cnode'] = app_data['cnode'].children[selectID]
+        app_data['history'] += app_data['children_name'][selectID] + '<br><br>'
+        app_data['children_name'] = [x.name for x in app_data['cnode'].children]
+    if not (request.form.get('reset') is None):
+        app_data['history'] = root.name + '<br><br>'
+        app_data['cnode'] = root
+        app_data['children_name'] = [x.name for x in root.children]
   return render_template('index.html', app_data=app_data)
 
 if __name__ == '__main__':
