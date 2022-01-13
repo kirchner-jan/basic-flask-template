b621ac76df07ccf0d1a8c05b57546cf9e955f61c
Jan Colab <jan040793gmail.com>
2022-01-13 21:58:23 +0000

    simplify

--- app.py
+++ app.py
@@ -2,40 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from anytree.importer import JsonImporter
+
+importer = JsonImporter()
+with open('/content/drive/MyDrive/g_projects/OMEN/stroco/tree_out.json') as f:
+  root = importer.read(f)
 
 DEVELOPMENT_ENV  = True
 
 app = Flask(__name__)
 
 app_data = {
-    "name":         "Peter's Starter Template for a Flask Web App",
-    "description":  "A basic Flask app using bootstrap for layout",
-    "author":       "Peter Simeth",
-    "html_title":   "Peter's Starter Template for a Flask Web App",
-    "project_name": "Starter Template",
-    "keywords":     "flask, webapp, template, basic"
+    "html_title":   "OMEN metaverse stroco",
+    "root": root.name
 }
 
 
 @app.route('/')
 def index():
     return render_template('index.html', app_data=app_data)
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html', app_data=app_data)
-
-
-@app.route('/service')
-def service():
-    return render_template('service.html', app_data=app_data)
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', app_data=app_data)
-
 
 if __name__ == '__main__':
     app.run(debug=DEVELOPMENT_ENV)
