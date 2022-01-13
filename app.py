68ac2aa643b9a2614fcbd3cfca3546b5c8638aaa
Jan Colab <jan040793gmail.com>
2022-01-13 23:07:26 +0000

    first working version

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 from flask_ngrok import run_with_ngrok
 from anytree.importer import JsonImporter
 
 importer = JsonImporter()
 with open('/content/drive/MyDrive/g_projects/OMEN/stroco/tree_out.json') as f:
   root = importer.read(f)
-
-DEVELOPMENT_ENV  = True
 
 app = Flask(__name__)
 run_with_ngrok(app)  # Start ngrok when app is run
@@ -19,10 +17,16 @@ app_data = {
     "root": root.name
 }
 
-
 @app.route('/')
 def index():
-    return render_template('index.html', app_data=app_data)
+  if request.method == 'POST':
+    if request.form.get('action1') == 'VALUE1':
+        pass # do something
+    elif  request.form.get('action2') == 'VALUE2':
+        pass # do something else
+    else:
+        pass # unknown
+  return render_template('index.html', app_data=app_data)
 
 if __name__ == '__main__':
-    app.run(debug=DEVELOPMENT_ENV)
+    app.run()
