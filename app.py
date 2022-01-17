437774e29be05fb5f674da6f993a7a98d91cc5a3
kirchnerj <kirchnerj@M-1766.local>
2022-01-17 22:04:25 +0000

    added sorting

--- app.py
+++ app.py
@@ -43,17 +43,20 @@ def index():
 def tree_explore(treeloc):
     print(treeloc)
     new_data = app_data.copy()
-    path = [int(a) for a in treeloc.split('/') if len(a) > 0] # if len(treeloc) > 9 else [int(treeloc)] 
+    path = [int(a) for a in treeloc.split('/') if len(a) > 0] # if len(treeloc) > 9 else [int(treeloc)]
     cNode = root
     history = cNode.name + '<br><br>'
     pathHist = ''
     for choice in path:
         cNode = cNode.children[choice]
-        
         history += '<a href="/explore/'+ pathHist +'">^</a> ' + cNode.name + '<br><br>' if len(pathHist) > 0 else '<a href="/'+ pathHist +'">^</a> ' + cNode.name + '<br><br>'
         pathHist += str(choice) + '/'
     new_data['history'] = history
+    s_ratings = np.array([x.s_rating for x in cNode.children])
+    g_ratings = np.array([x.g_rating for x in cNode.children])
+    t_rating = np.sqrt(s_ratings**2 + g_ratings**2)
     new_data['children_name'] = [x.name for x in cNode.children]
+    new_data['children_name'] = [x for _, x in sorted(zip(t_rating, new_data['children_name']))]
     new_data['treeloc'] = treeloc
     return render_template('tree_explore.html', new_data=new_data)
 
